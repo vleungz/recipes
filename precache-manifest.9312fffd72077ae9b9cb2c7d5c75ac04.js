638cf83236810eb84639b2ae0df013732ea800f8
Vanessa <vleungz.98@gmail.com>
2020-04-23 01:24:45 +0800

    Updates

---
+++ precache-manifest.9312fffd72077ae9b9cb2c7d5c75ac04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee42562eee08b0b8ee880f73998be6bd",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "303565b5dfd854e651a5",
+    "url": "/recipes/static/css/main.aa81f7af.chunk.css"
+  },
+  {
+    "revision": "de78434edc0889e56aa9",
+    "url": "/recipes/static/js/2.7ffb66ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.7ffb66ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303565b5dfd854e651a5",
+    "url": "/recipes/static/js/main.c609bfab.chunk.js"
+  },
+  {
+    "revision": "09393563c19f3daa0fb7",
+    "url": "/recipes/static/js/runtime-main.c53e652a.js"
+  }
+]);
